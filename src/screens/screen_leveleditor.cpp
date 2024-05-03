64014f02f9fa6747aba5ce26226dd4b336812ee7
dave-eska <dentes340@gmail.com>
2024-05-04 00:04:19 +0700

    Added mouse wheel to selecting current tile id

--- src/screens/screen_leveleditor.cpp
+++ src/screens/screen_leveleditor.cpp
@@ -187,26 +187,27 @@ void UpdateLevelEditorScreen(){
     }
 
     // Zoom based on mouse wheel
-    float wheel = GetMouseWheelMove();
-    if (wheel != 0)
-    {
-        // Get the world point that is under the mouse
-        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+    if(!IsKeyDown(KEY_LEFT_CONTROL)){
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0)
+        {
+            // Get the world point that is under the mouse
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
 
-        // Set the offset to where the mouse is
-        camera.offset = GetMousePosition();
+            // Set the offset to where the mouse is
+            camera.offset = GetMousePosition();
 
-        // Set the target to match, so that the camera maps the world space point 
-        // under the cursor to the screen space point under the cursor at any zoom
-        camera.target = mouseWorldPos;
+            // Set the target to match, so that the camera maps the world space point 
+            // under the cursor to the screen space point under the cursor at any zoom
+            camera.target = mouseWorldPos;
 
-        // Zoom increment
-        const float zoomIncrement = 0.125f;
+            // Zoom increment
+            const float zoomIncrement = 0.125f;
 
-        camera.zoom += (wheel*zoomIncrement);
-        if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+            camera.zoom += (wheel*zoomIncrement);
+            if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+        }
     }
-
 
     if(!is_typing){
         float inputX = IsKeyDown(KEY_D)-IsKeyDown(KEY_A);
@@ -220,6 +221,9 @@ void UpdateLevelEditorScreen(){
         clamp(currentTileID, 0, 22);
 
         if(IsKeyDown(KEY_LEFT_CONTROL)){
+            currentTileID += (GetMouseWheelMove() * 1);
+            if(GetMouseWheelMove() != 0) currentTileTexture = newItem(currentTileID).iconTexture;
+
             if(IsKeyPressed(KEY_S)){
                 savingCode();
             }
